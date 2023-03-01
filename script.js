2dacd734fe479a6aa1e6538de1ece410d6d7bcb2
Javaeria <javerianisar.1993@gmail.com>
2023-03-01 16:22:19 +1100

    Add play round function

--- script.js
+++ script.js
@@ -5,3 +5,26 @@ const GAME_RULE = { 1: "ROCK", 2: "PAPER", 3: "SCISSORS" };
 function getComputerChoice() {
   return GAME_RULE[Math.floor(Math.random() * 3) + 1];
 }
+
+function playRound(playerSelection, computerSelection) {
+  let result;
+  switch (true) {
+    case playerSelection === computerSelection:
+      result = 0;
+      break;
+    case (playerSelection === "ROCK" && computerSelection === "SCISSORS") ||
+      (playerSelection === "SCISSORS" && computerSelection === "PAPER") ||
+      (playerSelection === "PAPER" && computerSelection === "ROCK"):
+      result = 1;
+      break;
+    case (playerSelection === "SCISSORS" && computerSelection === "ROCK") ||
+      (playerSelection === "PAPER" && computerSelection === "SCISSORS") ||
+      (playerSelection === "ROCK" && computerSelection === "PAPER"):
+      result = 2;
+      break;
+    default:
+      result = -1;
+      break;
+  }
+  return result;
+}
